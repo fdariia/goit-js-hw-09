845ec05ef2da300182379f165d5727494e52d8d0
fdariia <fdariya3005@gmail.com>
2023-02-06 21:25:09 +0200

    task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('[name=delay]'),
+  step: document.querySelector('[name=step]'),
+  amount: document.querySelector('[name=amount]'),
+  button: document.querySelector('[type=submit]'),
+};
+
+refs.form.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+  let firstDelay = Number(refs.delay.value);
+  let delayStep = Number(refs.step.value);
+  let amount = Number(refs.amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, firstDelay)
+      .then(success => {
+        Notify.success(success);
+      })
+      .catch(error => {
+        Notify.failure(error);
+      });
+    firstDelay += delayStep;
+  }
+  event.currentTarget.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
